3f49b129456427ec00e24b756e873259e67fdf85
mangel27 <angel@tpix.in>
2023-08-18 16:37:20 +0530

    removed unused code

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://www.halaplay.com",
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://www.sportskeeda.com/",
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
   // ...other options
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,25 @@
-import { Box, Link, SimpleGrid, Text, Center, Flex, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Link,
+  SimpleGrid,
+  Text,
+  Center,
+  Flex,
+  Stack,
+} from "@chakra-ui/react";
 import NextImage from "next/image";
 import Gateway from "./Gateway";
 
 const Footer = () => {
   return (
     <Box bg="white">
-      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={6} textAlign={{ base: "left", md: "left" }} mx={50} mt={10}>
+      <SimpleGrid
+        columns={{ base: 1, md: 3 }}
+        spacing={6}
+        textAlign={{ base: "left", md: "left" }}
+        mx={50}
+        mt={10}
+      >
         <Center>
           <NextImage
             src="/sportkeeda_logo.jpg"
@@ -16,15 +30,28 @@ const Footer = () => {
           />
         </Center>
         <Center>
-          <Flex direction= "column" alignItems="left" mx={10} color="gray.600" lineHeight={8} fontWeight="bold">
+          <Flex
+            direction="column"
+            alignItems="left"
+            mx={10}
+            color="gray.600"
+            lineHeight={8}
+            fontWeight="bold"
+          >
             <Link href={"https://www.sportskeeda.com/about-us"}>About Us</Link>
-            <Link href={"https://www.sportskeeda.com/privacy-policy"}>Privacy Policy</Link>
-            <Link href={"https://www.sportskeeda.com/terms-of-use"}>Terms of Service</Link>
+            <Link href={"https://www.sportskeeda.com/privacy-policy"}>
+              Privacy Policy
+            </Link>
+            <Link href={"https://www.sportskeeda.com/terms-of-use"}>
+              Terms of Service
+            </Link>
             <Link href={"https://www.sportskeeda.com/contact"}>Contact Us</Link>
-            <Link href={"https://www.sportskeeda.com/gdpr-compliance"}>GDPR</Link>
+            <Link href={"https://www.sportskeeda.com/gdpr-compliance"}>
+              GDPR
+            </Link>
           </Flex>
         </Center>
-        <Center justify={["start" ,"center"]}>
+        <Center justify={["start", "center"]}>
           <Stack direction="column" spacing={4} mt={[6, 0]}>
             <Link
               href="https://play.google.com/store/apps/details?id=com.sportskeeda.topic"
